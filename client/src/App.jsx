0b60c697f4218d19467be42b9a86d364162ed92c
Vibinchandar <vibinmarutharajavp@gmail.com>
2026-04-06 10:34:58 +0530

    Included client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './tailwind.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="text-blue-500">Task Management</h1>
+    </div>
+  );
+}
+
+export default App;
